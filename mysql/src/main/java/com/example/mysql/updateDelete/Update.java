e413d7b4695ed7e10dfa9baa23693ad84d71ec47
RAMANJANEYULU <ramanjaneyulu.randhi@healthcarenow.in>
2024-06-01 17:24:00 +0530

    mysql first commit

---
+++ mysql/src/main/java/com/example/mysql/updateDelete/Update.java
@@ -0,0 +1,15 @@
+package com.example.mysql.updateDelete;
+
+public class Update {
+	
+	/*
+	* update in mysql is used the update data inside mysql tables
+	* 
+	* syntax - update table_name set column_name = some value where id(or any column reference) = value;
+	* multi columns update -> update table_name set  column_name = some value, column_name = some value where id = value;
+	*
+	*
+	*
+	*/
+
+}
